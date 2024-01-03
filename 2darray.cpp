588bd096b46c891430bfe3720e0ae5f486d47d80
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-01-03 09:05:52 +0600

    strings are done

--- 2darray.cpp
+++ 2darray.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 
 int main(){
-    int arr[3][5];
+    // int arr[3][5];
 
-    arr[1][3] = 78;
-    cout << arr[1][3];
+    // arr[1][3] = 78;
+    // cout << arr[1][3];
+
+    string s = "Striver";
+    int len = s.size();
+    cout << s[len-1];
     return 0;
 }
