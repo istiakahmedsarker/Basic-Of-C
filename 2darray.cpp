d94e3cfbba7f8f918c3af9930ceb4371c23f9035
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-01-03 10:20:26 +0600

    passed by reference added

--- 2darray.cpp
+++ 2darray.cpp
@@ -1,14 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
+void printName(string &name){
+    name = 'is';
+    cout << name;
+}
 int main(){
     // int arr[3][5];
 
     // arr[1][3] = 78;
     // cout << arr[1][3];
 
-    string s = "Striver";
-    int len = s.size();
-    cout << s[len-1];
+    // string s = "Striver";
+    // int len = s.size();
+    // cout << s[len-1];
+    string name = "Istiak";
+    printName(name);
+
     return 0;
 }
